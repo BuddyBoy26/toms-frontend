4ea157132a51861bfff806b1b9db02ce997d8f81
Buddyboyps26 <pritvikps26@gmail.com>
2025-08-23 00:24:23 +0530

    Presentation 1 23 8: apiConstants corrected

--- src/constants/apiConstants.ts
+++ src/constants/apiConstants.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_API_URL = 'https://toms-backend-a7ot.onrender.com/api'
-export const BACKEND_API_URL = 'http://localhost:8000/api'
+export const BACKEND_API_URL = 'https://toms-backend-a7ot.onrender.com/api'
+// export const BACKEND_API_URL = 'http://localhost:8000/api'
